e5547633c33a20dde10a0297d664c9b6019500e3
PaulPidou <pidou.paul@gmail.com>
2020-04-23 18:31:34 +0200

    Add tfjs

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
 import AppNavigator from "./navigation/AppNavigator";
 
-
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTfReady: false,
+        };
+    }
+
+    async componentDidMount() {
+        // Wait for tf to be ready.
+        await tf.ready();
+        // Signal to the app that tensorflow.js can now be used.
+        this.setState({
+            isTfReady: true,
+        });
+    }
+
     render() {
         return (<AppNavigator />);
     }
