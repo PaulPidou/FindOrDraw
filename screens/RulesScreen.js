14fac17384c936dfa6ee5bf626b9d6bf0d4d864d
theod <theo.depresle@gmail.com>
2020-04-27 21:11:47 +0200

    Adding all the screens

---
+++ screens/RulesScreen.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Button, StatusBar, Image} from 'react-native';
+import Constants from "expo-constants";
+
+import * as Colors from "../constants/Constants";
+import logo_bleu from "../assets/logo_transparent_vert.png"
+import Text from "../components/Text";
+
+export default class RulesScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle='light-content' backgroundColor={"rgba(0,0,0,0)"} translucent={true}/>
+
+                <View>
+                    <Text>Rules !</Text>
+                </View>
+            </View>)
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.BackgroundColor,
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        paddingTop: Constants.statusBarHeight,
+        paddingHorizontal: 20
+    },
+
+});
