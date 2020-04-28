1ba5d04b6fa265e2b5b0cb95c64c66c6e0b93beb
PaulPidou <pidou.paul@gmail.com>
2020-04-28 16:58:03 +0200

    Functional FindScreen

--- screens/FindScreen.js
+++ screens/FindScreen.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {ActivityIndicator, Button, StyleSheet, View, Platform } from 'react-native';
-import Svg, { Circle, Rect, G, Line} from 'react-native-svg';
+import {ActivityIndicator, StyleSheet, View, Platform, Text } from 'react-native';
 
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
-import { ExpoWebGLRenderingContext } from 'expo-gl';
 
 import * as tf from '@tensorflow/tfjs';
-import * as blazeface from '@tensorflow-models/blazeface';
-import * as posenet from '@tensorflow-models/posenet';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 import {cameraWithTensors} from '@tensorflow/tfjs-react-native';
 
 const inputTensorWidth = 152;
@@ -25,54 +22,28 @@ export default class FindScreen extends React.Component {
         super(props);
         this.state = {
             isLoading: true,
-            cameraType: Camera.Constants.Type.front,
-            modelName: 'posenet',
+            cameraType: Camera.Constants.Type.back,
+            results: []
         };
         this.handleImageTensorReady = this.handleImageTensorReady.bind(this);
     }
 
-    async loadPosenetModel() {
-        const model =  await posenet.load({
-            architecture: 'MobileNetV1',
-            outputStride: 16,
-            inputResolution: { width: inputTensorWidth, height: inputTensorHeight },
-            multiplier: 0.75,
-            quantBytes: 2
-        });
-        return model;
-    }
-
-    async loadBlazefaceModel() {
-        const model = await blazeface.load();
-        return model;
+    async loadMobilenetModel() {
+        return await mobilenet.load()
     }
 
     async handleImageTensorReady(images, updatePreview, gl) {
         const loop = async () => {
-            const {modelName} = this.state;
             if(!AUTORENDER) {
                 updatePreview();
             }
 
-            if(modelName === 'posenet') {
-                if (this.state.posenetModel != null) {
-                    const imageTensor = images.next().value;
-                    const flipHorizontal = Platform.OS === 'ios' ? false : true;
-                    const pose = await this.state.posenetModel.estimateSinglePose(
-                        imageTensor, { flipHorizontal });
-                    this.setState({pose});
-                    tf.dispose([imageTensor]);
-                }
-            } else {
-                if (this.state.faceDetector != null) {
-                    const imageTensor = images.next().value;
-                    const returnTensors = false;
-                    const faces = await this.state.faceDetector.estimateFaces(
-                        imageTensor, returnTensors);
+            if (this.state.mobilenetModel != null) {
+                const imageTensor = images.next().value;
+                const prediction = await this.state.mobilenetModel.classify(imageTensor);
 
-                    this.setState({faces});
-                    tf.dispose(imageTensor);
-                }
+                this.setState({results: prediction});
+                tf.dispose(imageTensor);
             }
 
             if(!AUTORENDER) {
@@ -80,7 +51,6 @@ export default class FindScreen extends React.Component {
             }
             this.rafID = requestAnimationFrame(loop);
         };
-
         loop();
     }
 
@@ -93,103 +63,18 @@ export default class FindScreen extends React.Component {
     async componentDidMount() {
         const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
-        const [blazefaceModel, posenetModel] =
-            await Promise.all([this.loadBlazefaceModel(), this.loadPosenetModel()]);
+        const [mobilenetModel] =
+            await Promise.all([this.loadMobilenetModel()]);
 
         this.setState({
             hasCameraPermission: status === 'granted',
             isLoading: false,
-            faceDetector: blazefaceModel,
-            posenetModel,
+            mobilenetModel
         });
     }
 
-    renderPose() {
-        const MIN_KEYPOINT_SCORE = 0.2;
-        const {pose} = this.state;
-        if (pose != null) {
-            const keypoints = pose.keypoints
-                .filter(k => k.score > MIN_KEYPOINT_SCORE)
-                .map((k,i) => {
-                    return <Circle
-                        key={`skeletonkp_${i}`}
-                        cx={k.position.x}
-                        cy={k.position.y}
-                        r='2'
-                        strokeWidth='0'
-                        fill='blue'
-                    />;
-                });
-
-            const adjacentKeypoints =
-                posenet.getAdjacentKeyPoints(pose.keypoints, MIN_KEYPOINT_SCORE);
-
-            const skeleton = adjacentKeypoints.map(([from, to], i) => {
-                return <Line
-                    key={`skeletonls_${i}`}
-                    x1={from.position.x}
-                    y1={from.position.y}
-                    x2={to.position.x}
-                    y2={to.position.y}
-                    stroke='magenta'
-                    strokeWidth='1'
-                />;
-            });
-
-            return <Svg height='100%' width='100%'
-                        viewBox={`0 0 ${inputTensorWidth} ${inputTensorHeight}`}>
-                {skeleton}
-                {keypoints}
-            </Svg>;
-        } else {
-            return null;
-        }
-    }
-
-    renderFaces() {
-        const {faces} = this.state;
-        if(faces != null) {
-            const faceBoxes = faces.map((f, fIndex) => {
-                const topLeft = f.topLeft;
-                const bottomRight = f.bottomRight;
-
-                const landmarks = f.landmarks.map((l, lIndex) => {
-                    return <Circle
-                        key={`landmark_${fIndex}_${lIndex}`}
-                        cx={l[0]}
-                        cy={l[1]}
-                        r='2'
-                        strokeWidth='0'
-                        fill='blue'
-                    />;
-                });
-
-                return <G key={`facebox_${fIndex}`}>
-                    <Rect
-                        x={topLeft[0]}
-                        y={topLeft[1]}
-                        fill={'red'}
-                        fillOpacity={0.2}
-                        width={(bottomRight[0] - topLeft[0])}
-                        height={(bottomRight[1] - topLeft[1])}
-                    />
-                    {landmarks}
-                </G>;
-            });
-
-            const flipHorizontal = Platform.OS === 'ios' ? 1 : -1;
-            return <Svg height='100%' width='100%'
-                        viewBox={`0 0 ${inputTensorWidth} ${inputTensorHeight}`}
-                        scaleX={flipHorizontal}>
-                {faceBoxes}
-            </Svg>;
-        } else {
-            return null;
-        }
-    }
-
     render() {
-        const {isLoading, modelName} = this.state;
+        const {isLoading} = this.state;
 
         // TODO File issue to be able get this from expo.
         // Caller will still need to account for orientation/phone rotation changes
@@ -221,22 +106,20 @@ export default class FindScreen extends React.Component {
                 onReady={this.handleImageTensorReady}
                 autorender={AUTORENDER}
             />
-            <View style={styles.modelResults}>
-                {modelName === 'posenet' ? this.renderPose() : this.renderFaces()}
-            </View>
         </View>;
 
         return (
             <View style={{width:'100%'}}>
-                <View style={styles.sectionContainer}>
-                    <Button
-                        onPress={this.props.returnToMain}
-                        title='Back'
-                    />
-                </View>
                 {isLoading ? <View style={[styles.loadingIndicator]}>
                     <ActivityIndicator size='large' color='#FF0266' />
                 </View> : camView}
+                {
+                    this.state.results.length > 0 && (
+                        <View style={{flex: 1}}>
+                            <Text>{this.state.results[0].className}</Text>
+                        </View>
+                    )
+                }
             </View>
         );
     }
@@ -245,9 +128,6 @@ export default class FindScreen extends React.Component {
 
 const styles = StyleSheet.create({
     loadingIndicator: {
-        position: 'absolute',
-        top: 20,
-        right: 20,
         zIndex: 200,
     },
     sectionContainer: {
@@ -259,9 +139,10 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#fff',
+        //width: '100%',
+        //height: '100%',
+        flex: 1,
+        backgroundColor: '#888',
     },
     camera : {
         position:'absolute',
@@ -270,17 +151,6 @@ const styles = StyleSheet.create({
         width: 600/2,
         height: 800/2,
         zIndex: 1,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 0,
-    },
-    modelResults: {
-        position:'absolute',
-        left: 50,
-        top: 100,
-        width: 600/2,
-        height: 800/2,
-        zIndex: 20,
         borderWidth: 1,
         borderColor: 'black',
         borderRadius: 0,
